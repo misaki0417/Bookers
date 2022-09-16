0a33fe1aea019f83b6342e8cc26afe728bc72149
misaki0417 <110611390+misaki0417@users.noreply.github.com>
2022-09-16 17:55:59 +0000

    [finish]

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get'books/new'
+  get'books'=>'books#index'
+  get'books/:id'=>'books#show',as:'show_books'
+  get'books/:id/edit'=>'books#edit',as:'edit_books'
+  post'/books'=>'books#create'
+  root :to=>'homes#top'
+  patch'books/:id'=>'books#update',as:'update_books'
+  delete'books/:id'=>'books#destroy',as:'destroy_books'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
