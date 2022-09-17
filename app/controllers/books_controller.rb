c7b09221ed948cd832db0e0f3482aca8fbe55034
misaki0417 <110611390+misaki0417@users.noreply.github.com>
2022-09-17 08:13:12 +0000

    [finish]RSpecによるテスト終了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,7 +10,7 @@ class BooksController < ApplicationController
 
    if @book.save
     flash[:notice]="Book was successfully created."
-    redirect_to show_books_path(@book.id)
+    redirect_to book_path(@book.id)
    else
     render :new
    end
@@ -24,26 +24,25 @@ class BooksController < ApplicationController
 
  def show
   @book=Book.find(params[:id])
-   end
+ end
 
  def edit
   @book=Book.find(params[:id])
-  
+
  end
 
 
-def update
- @book=Book.find(params[:id])
- @book.update(book_params)
+ def update
+  @book=Book.find(params[:id])
+  @book.update(book_params)
 
- if @book.update(book_params)
+  if @book.update(book_params)
   flash[:notice]="Book was successfully updated."
-  redirect_to show_books_path(@book.id)
+  redirect_to book_path(@book.id)
   else
    render :edit
+  end
  end
-
-end
 
 def destroy
  book=Book.find(params[:id])
@@ -57,4 +56,6 @@ end
  def book_params
   params.require(:book).permit(:title,:body)
  end
- end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
-  get'books/new'
-  get'books'=>'books#index'
-  get'books/:id'=>'books#show',as:'show_books'
-  get'books/:id/edit'=>'books#edit',as:'edit_books'
-  post'/books'=>'books#create'
+  # get'books/new'
+  # get'books'=>'books#index'
+  # get'books/:id'=>'books#show',as:'show_books'
+  # get'books/:id/edit'=>'books#edit',as:'edit_books'
+  # post'/books'=>'books#create'
   root :to=>'homes#top'
-  patch'books/:id'=>'books#update',as:'update_books'
-  delete'books/:id'=>'books#destroy',as:'destroy_books'
+  # patch'books/:id'=>'books#update',as:'update_books'
+  # delete'books/:id'=>'books#destroy',as:'destroy_books'
+
+  resources :books
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
